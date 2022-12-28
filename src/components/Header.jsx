87456aac2fa13191a1c1b9854b382790f2435a50
joo <sejuyes@gmail.com>
2022-12-28 18:16:07 +0900

    Feat: 다크모드 여부를 위한 Context 추가

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
-import { BsFillMoonFill } from "react-icons/bs";
+import React, { useContext, useState } from "react";
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import { DarkModeContext } from "../context/DarkModeContext";
 import styles from "../styles/Header.module.css";
 
 export default function Header() {
   const [selected, setSelected] = useState(false);
+  const { darkMode, toggleDarkMode } = useContext(DarkModeContext);
   return (
     <div className={styles.header}>
-      <div className={styles.mode}>
-        <BsFillMoonFill />
+      <div className={styles.mode} onClick={toggleDarkMode}>
+        {darkMode ? <BsFillSunFill /> : <BsFillMoonFill />}
       </div>
       <div className={styles.filter}>
         <span>
