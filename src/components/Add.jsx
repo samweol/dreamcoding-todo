0b668cab4177df5a104b8f2cf4c09c48994ae7a8
samweol <sejuyes@gmail.com>
2022-12-30 17:50:07 +0900

    Refactor: 컴포넌트 props, 함수로빼기 등 진행

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useContext } from "react";
 import styles from "../styles/Add.module.css";
 import { DarkModeContext } from "../context/DarkModeContext";
+import { v4 as uuidv4 } from "uuid";
 
-export default function Add({ list, setList }) {
+export default function Add({ onAdd }) {
   const [title, setTitle] = useState("");
   const { darkMode } = useContext(DarkModeContext);
 
@@ -10,27 +11,30 @@ export default function Add({ list, setList }) {
     setTitle(e.target.value);
   };
 
-  const addToDoList = () => {
-    setList([...list, { title: title, isChecked: false }]);
-    const input = document.getElementById("textField");
-    input.value = null;
+  const handleSubmit = (e) => {
+    e.preventDefault(); //새로고침 방지
+    if (title.trim().length === 0) {
+      return;
+    }
+    onAdd({ id: uuidv4(), title, status: "active" });
+    setTitle("");
   };
 
   return (
     <div className={darkMode ? styles.dark : styles.light}>
-      <input
-        type="text"
-        className={styles.textField}
-        placeholder="Add Todo"
-        onChange={handleChange}
-        id="textField"
-      />
-      <button
-        className={darkMode ? styles.darkButton : styles.lightButton}
-        onClick={addToDoList}
-      >
-        Add
-      </button>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className={styles.textField}
+          placeholder="Add Todo"
+          onChange={handleChange}
+          id="textField"
+          value={title}
+        />
+        <button className={darkMode ? styles.darkButton : styles.lightButton}>
+          Add
+        </button>
+      </form>
     </div>
   );
 }
